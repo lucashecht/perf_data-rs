a110fb12b637c4d0fa106f0eb4c5cd2b22e5972a
Lucas Hecht <lucas.hecht@cyberus-technology.de>
2021-12-13 17:24:26 +0100

    Kill QEMU after profiling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-use std::process::{Command, Stdio};
+use std::process::{Command, Child, Stdio};
 use std::io::{BufRead, BufReader, Error, ErrorKind};
 
 mod perf_data;
 
 fn main() -> Result<(), Error> {
-    let stdout = Command::new("qemu-system-x86_64")
-        .args(["-m", "2048", "-M", "q35", "-cpu", "host,+vmx", "-enable-kvm", "-smp", "cores=4,threads=1,sockets=1"])
-        .args(["-serial", "stdio", "-k", "en-us"])
-        .args(["-kernel", "/home/lhecht/Downloads/bender"])
-        .args(["-initrd", "/home/lhecht/git/supernova-core/build/share/hedron/hypervisor novga serial spinner,/home/lhecht/git/supernova-core/build/test/roottask/thesis-impl_roottasktest"])
-        .stdout(Stdio::piped())
-        .spawn()?
-        .stdout
-        .ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))?;
+    let mut proc = run_qemu();
+
+    let stdout = proc.stdout.take().ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))?;
 
     let reader = BufReader::new(stdout);
     let mut lines = reader.lines().filter_map(|line| line.ok());
@@ -28,5 +22,19 @@ fn main() -> Result<(), Error> {
             println!("{:#16x}", i64::from_str_radix(&line[6..], 16).unwrap());
         }
     }
+    proc.kill();
+
     Ok(())
 }
+
+fn run_qemu() -> Child {
+     Command::new("qemu-system-x86_64")
+        .args(["-m", "2048", "-M", "q35", "-cpu", "host,+vmx", "-enable-kvm", "-smp", "cores=4,threads=1,sockets=1"])
+        .args(["-serial", "stdio", "-k", "en-us"])
+        .args(["-kernel", "/home/lhecht/Downloads/bender"])
+        .args(["-initrd", "/home/lhecht/git/supernova-core/build/share/hedron/hypervisor novga serial spinner,/home/lhecht/git/supernova-core/build/test/roottask/thesis-impl_roottasktest"])
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to execute QEMU")
+}
+
