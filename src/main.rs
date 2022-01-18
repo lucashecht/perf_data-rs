7ec8510f7194d6b16d6aeb64b1d85bc01cda9f52
Lucas Hecht <lucas.hecht@cyberus-technology.de>
2022-01-18 15:33:20 +0100

    Calculate function shares of runtime

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,14 @@ mod perf_data;
 fn main() -> Result<(), Error> {
     let samples: Vec<u64> = vec![0x1110, 0x1116, 0x111a, 0x111c];
 
-    let samples = collect_samples();
-    create_perf_file(samples);
+    loop {
+        let samples = collect_samples();
+        println!("{}", samples.len());
+        println!("fun1: {:.2}%", samples.iter().filter(|&x| x>=&0x1000000000C0 && x<&0x1000000000F0).count() as f64 / samples.len() as f64 * 100f64);
+        println!("fun2: {:.2}%", samples.iter().filter(|&x| x>=&0x1000000000F0 && x<&0x100000000120).count() as f64 / samples.len() as f64 * 100f64);
+        println!("fun3: {:.2}%", samples.iter().filter(|&x| x>=&0x100000000120 && x<&0x100000000150).count() as f64 / samples.len() as f64 * 100f64);
+    }
+    //create_perf_file(samples);
 
     Ok(())
 }
@@ -27,7 +33,7 @@ fn collect_samples() -> Vec<u64> {
             break;
         } else if line.find("IP: ").is_some() {
             let sample = u64::from_str_radix(&line[6..], 16).unwrap();
-            println!("{:#16x}", sample);
+            //println!("{:#16x}", sample);
             samples.push(sample);
         }
     }
@@ -39,14 +45,14 @@ fn create_perf_file(samples: Vec<u64>) {
     let header_size = mem::size_of::<perf_data::PerfHeader>();
 
     let attr = perf_data::PerfEventAttr::default();
-    let mut data_mmap = vec![perf_data::RecordMmap::default(); 1];
-    let filename = "matrix_multiply";
-    data_mmap[0].filename.copy_from_slice(format!("{:\0<width$}", filename, width = perf_data::PATH_MAX).as_bytes()); 
+    let filename = "/home/lhecht/git/analysis-tool/src/matrix_multiply";
+    let mut record_mmap = perf_data::RecordMmap::default();
+    record_mmap.filename.copy_from_slice(format!("{:\0<width$}", filename, width = perf_data::PATH_MAX).as_bytes());
     let data_sample: Vec<perf_data::RecordSample> = samples.iter()
                              .enumerate()
-                             .map(|(i, ip)| perf_data::RecordSample { ip: *ip, id: i as u64, ..Default::default() })
+                             .map(|(i, ip)| perf_data::RecordSample { ip: *ip, ..Default::default() })
                              .collect();
-    let comm = perf_data::RecordComm { comm: data_mmap[0].filename, ..Default::default() };
+    let comm = perf_data::RecordComm { comm: record_mmap.filename, ..Default::default() };
 
     let header = perf_data::PerfHeader {
         magic: *b"PERFILE2",
@@ -58,7 +64,7 @@ fn create_perf_file(samples: Vec<u64>) {
         },
         data: perf_data::PerfFileSection {
             offset: (header_size + mem::size_of::<perf_data::PerfEventAttr>()) as u64,
-            size: (mem::size_of_val(&*data_mmap)+mem::size_of_val(&*data_sample)+mem::size_of_val(&comm)) as u64,
+            size: (mem::size_of_val(&record_mmap)+mem::size_of_val(&*data_sample)+mem::size_of_val(&comm)) as u64,
         },
         ..Default::default()
     };
@@ -80,7 +86,7 @@ fn create_perf_file(samples: Vec<u64>) {
         // write header, attribute section and data section to file
         write_struct(&mut file, &header);
         write_struct(&mut file, &attr);
-        write_vector(&mut file, data_mmap);
+        write_struct(&mut file, &record_mmap);
         write_struct(&mut file, &comm);
         write_vector(&mut file, data_sample);
     }
